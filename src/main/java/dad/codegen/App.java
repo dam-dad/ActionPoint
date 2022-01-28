eddfcbb82a9345b9f2eda902d0b26d84289b037b
SamuelGlezMrt <71700577+SamuelGlezMrt@users.noreply.github.com>
2022-01-28 11:09:40 +0000

    Update App.java

--- src/main/java/dad/codegen/App.java
+++ src/main/java/dad/codegen/App.java
@@ -1,16 +1,16 @@
 package dad.codegen;
 
-import dad.controller.RootController;
+import dad.controller.iniciarController;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+public class App extends Application {
+	private iniciarController root;
 
-public class App extends Application{
-private RootController root;
-public void start(Stage primaryStage) throws Exception {
-		root=new RootController();
-		Scene scene=new Scene(root.getView());
+	public void start(Stage primaryStage) throws Exception {
+		root = new iniciarController();
+		Scene scene = new Scene(root.getView());
 		primaryStage.setTitle("2");
 		primaryStage.setScene(scene);
 		primaryStage.show();
