c31a8c30b6d55dabbe7ceb5d17a5c9583729846c
GabrielPerezMinik <91058454+GabrielPerezMinik@users.noreply.github.com>
2022-02-08 11:22:39 +0000

    Login #1

---
+++ src/main/java/login/Model.java
@@ -0,0 +1,26 @@
+package login;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+
+public class Model {
+
+
+	    @FXML
+	    private PasswordField contraText;
+
+	    @FXML
+	    private Label mensajeLabel;
+
+	    @FXML
+	    private Button nuevaCuentaButon;
+
+	    @FXML
+	    private TextField usuarioText;
+	
+	
+}
