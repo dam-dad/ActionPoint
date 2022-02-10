15baecf9eb064ea4fbb367e94935f241e6fe1e04
fvarrui <4354486+fvarrui@users.noreply.github.com>
2022-02-10 13:15:27 +0000

    refactorizacion

--- src/main/java/newsapilib/Main.java
+++ src/main/java/newsapilib/Main.java
@@ -1,76 +1,64 @@
 package newsapilib;
 
-import newsapilib.models.request.EverythingRequest;
-import newsapilib.models.request.SourcesRequest;
-import newsapilib.models.request.TopHeadlinesRequest;
-import newsapilib.models.response.ArticleResponse;
-import newsapilib.models.response.SourcesResponse;
+import com.kwabenaberko.newsapilib.NewsApiClient;
+import com.kwabenaberko.newsapilib.models.Article;
+import com.kwabenaberko.newsapilib.models.Source;
+import com.kwabenaberko.newsapilib.models.request.EverythingRequest;
+import com.kwabenaberko.newsapilib.models.request.SourcesRequest;
+import com.kwabenaberko.newsapilib.models.request.TopHeadlinesRequest;
+import com.kwabenaberko.newsapilib.models.response.ArticleResponse;
+import com.kwabenaberko.newsapilib.models.response.SourcesResponse;
 
 public class Main {
 
 	public static void main(String[] args) {
+
 		NewsApiClient newsApiClient = new NewsApiClient("900c5aa5b579479b98c951399bf6adb1");
 
+		NewsApiClient.ArticlesResponseCallback callback = new NewsApiClient.ArticlesResponseCallback() {
+			@Override
+			public void onSuccess(ArticleResponse response) {
+				System.out.println("Ya tengo un resultado!!!! " + response.getArticles().size());
+				for (Article article : response.getArticles()) {
+					System.out.println("----------" + article.getTitle() + "-------------");
+					System.out.println(article.getDescription());
+					System.out.println(article.getAuthor());
+					System.out.println(article.getPublishedAt());
+					System.out.println(article.getUrl());
+				}
+			}
+
+			@Override
+			public void onFailure(Throwable throwable) {
+				System.err.println(throwable.getMessage());
+			}
+		};
+
 		// /v2/everything
-		newsApiClient.getEverything(
-		  new EverythingRequest.Builder()
-		          .q("trump")
-		          .language("es")
-		          .build(),
-		  new NewsApiClient.ArticlesResponseCallback() {
-		      @Override
-		      public void onSuccess(ArticleResponse response) {
-		          System.out.println(response.getArticles().get(0).getTitle());
-		          System.out.println(response.getArticles().get(0).getDescription());
-		          System.out.println(response.getArticles().get(0).getAuthor());  
-		          System.out.println(response.getArticles().get(0).getPublishedAt());
-		          System.out.println(response.getArticles().get(0).getUrl());
-		      }
+		newsApiClient.getEverything(new EverythingRequest.Builder().q("reforma laboral").language("es").pageSize(5).page(2).sortBy("publishedAt").build(), callback);
 
-		      @Override
-		      public void onFailure(Throwable throwable) {
-		          System.out.println(throwable.getMessage());
-		      }
-		  }
-		);
-		        
-//		// /v2/top-headlines
-//		newsApiClient.getTopHeadlines(
-//		  new TopHeadlinesRequest.Builder()
-//		    .q("bitcoin")
-//		    .build(),
-//		  new NewsApiClient.ArticlesResponseCallback() {
-//		    @Override
-//		    public void onSuccess(ArticleResponse response) {
-//		      System.out.println(response.getArticles().get(0).getTitle());
-//		    }
+		// /v2/top-headlines
+//		newsApiClient.getTopHeadlines(new TopHeadlinesRequest.Builder().q("reforma loboral").language("es").build(), callback);
+
+		// /v2/top-headlines/sources
+//		newsApiClient.getSources(new SourcesRequest.Builder().build(),
+//				new NewsApiClient.SourcesCallback() {
+//					@Override
+//					public void onSuccess(SourcesResponse response) {
+//						System.out.println("Ya tengo un resultado!!!!");
+//						for (Source source : response.getSources()) {
+//							System.out.println("----------" + source.getName() + "-------------");
+//							System.out.println(source.getDescription());
+//							System.out.println(source.getUrl());
+//						}					
+//					}
 //
-//		    @Override
-//		    public void onFailure(Throwable throwable) {
-//		      System.out.println(throwable.getMessage());
-//		    }
-//		  }
-//		);
-//		        
-//		// /v2/top-headlines/sources
-//		newsApiClient.getSources(
-//		  new SourcesRequest.Builder()
-//		    .language("en")
-//		    .country("us")
-//		    .build(),
-//		  new NewsApiClient.SourcesCallback() {
-//		    @Override
-//		    public void onSuccess(SourcesResponse response) {
-//		        System.out.println(response.getSources().get(0).getName());
-//		    }
-//
-//		    @Override
-//		    public void onFailure(Throwable throwable) {
-//		      System.out.println(throwable.getMessage());
-//		    }
-//		  }
-//		);
+//					@Override
+//					public void onFailure(Throwable throwable) {
+//						System.out.println(throwable.getMessage());
+//					}
+//				});
+
 	}
-	
-}
 
+}
