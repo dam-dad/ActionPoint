fcdb81beb557e398f2f9efd821de3c2a4dcd6066
AdrianNietoRamos <adrianietoemfb@gmail.com>
2022-02-19 19:15:47 +0000

    Update NoticiasController.java

--- src/main/java/dad/controller/NoticiasController.java
+++ src/main/java/dad/controller/NoticiasController.java
@@ -10,12 +10,16 @@ import com.kwabenaberko.newsapilib.models.Article;
 import com.kwabenaberko.newsapilib.models.request.EverythingRequest;
 import com.kwabenaberko.newsapilib.models.response.ArticleResponse;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
 
 public class NoticiasController implements Initializable {
 
@@ -29,10 +33,46 @@ public class NoticiasController implements Initializable {
 	private GridPane RootGridPane;
 
 	@FXML
-	private ImageView imagen1ImageView;
+	private Button boton;
 
 	@FXML
-	private TextArea primerTextArea;
+	private ImageView imagen;
+
+	@FXML
+	private ImageView imagen1;
+	@FXML
+	private ImageView imagen11;
+
+	@FXML
+	private Text tituloText;
+
+	@FXML
+	private Text descriptionText;
+
+	@FXML
+	private Text publicatedText;
+
+	@FXML
+	private Text tituloText1;
+
+	@FXML
+	private Text descriptionText1;
+
+	@FXML
+	private Text publicatedText1;
+	@FXML
+	private Text tituloText11;
+
+	@FXML
+	private Text descriptionText11;
+
+	@FXML
+	private Text publicatedText11;
+
+	@FXML
+	void onBoton(ActionEvent event) {
+
+	}
 
 	public NoticiasController() throws IOException {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/NoticiasView.fxml"));
@@ -44,8 +84,6 @@ public class NoticiasController implements Initializable {
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 
-		primerTextArea.setText(dad.actionpoint.newsapi.Main.getTitulo());
-		System.out.println(dad.actionpoint.newsapi.Main.getTitulo());
 		
 
 		NewsApiClient.ArticlesResponseCallback callback = new NewsApiClient.ArticlesResponseCallback() {
@@ -66,7 +104,23 @@ public class NoticiasController implements Initializable {
 	
 	private  void cargarDatos(List<Article> articles) {
 		
-		primerTextArea.setText(articles.get(0).getDescription());
+		tituloText.setText(articles.get(0).getTitle());
+		descriptionText.setText(articles.get(0).getDescription());
+		publicatedText.setText(articles.get(0).getAuthor() + " " + articles.get(0).getPublishedAt());
+		Image image = new Image(articles.get(0).getUrlToImage());
+		imagen.setImage(image);
+
+		tituloText1.setText(articles.get(1).getTitle());
+		descriptionText1.setText(articles.get(1).getDescription());
+		publicatedText1.setText(articles.get(1).getAuthor() + " " + articles.get(1).getPublishedAt());
+		Image image1 = new Image(articles.get(1).getUrlToImage());
+		imagen1.setImage(image1);
+
+		tituloText11.setText(articles.get(2).getTitle());
+		descriptionText11.setText(articles.get(2).getDescription());
+		publicatedText11.setText(articles.get(2).getAuthor() + " " + articles.get(2).getPublishedAt());
+		Image image11 = new Image(articles.get(2).getUrlToImage());
+		imagen11.setImage(image11);
 		
 	}
 
@@ -78,20 +132,6 @@ public class NoticiasController implements Initializable {
 		RootGridPane = rootGridPane;
 	}
 
-	public ImageView getImagen1ImageView() {
-		return imagen1ImageView;
-	}
-
-	public void setImagen1ImageView(ImageView imagen1ImageView) {
-		this.imagen1ImageView = imagen1ImageView;
-	}
-
-	public TextArea getPrimerTextArea() {
-		return primerTextArea;
-	}
-
-	public void setPrimerTextArea(TextArea primerTextArea) {
-		this.primerTextArea = primerTextArea;
-	}
+	
 
 }
