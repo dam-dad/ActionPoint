afde4644f05e5c9f4990c39ce9e4bc60cd5b7036
AdrianNietoRamos <adrianietoemfb@gmail.com>
2022-02-23 12:46:23 +0000

    Update NoticiaWebController.java

--- src/main/java/dad/controller/NoticiaWebController.java
+++ src/main/java/dad/controller/NoticiaWebController.java
@@ -1,40 +1,83 @@
 package dad.controller;
 
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.web.WebEngine;
 import javafx.scene.web.WebView;
 
 public class NoticiaWebController implements Initializable{
 
-    @FXML
-    private BorderPane noticiaWebBorderPane;
+	private StringProperty url = new SimpleStringProperty();
 
-    @FXML
-    private WebView noticiaWebView;
+	public NoticiaWebController() throws IOException {
 
-   
-    
-    
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/WebNoticiasView.fxml"));
+		loader.setController(this);
+		loader.load();
+
+	}
+
+	@FXML
+	private AnchorPane view;
+
+	@FXML
+	private WebView webView;
+
+	@FXML
+	private Button loadButton;
+
+	@FXML
+	private TextField textField;
+
+	private WebEngine engine;
+
+	@FXML
+	void onLoad(ActionEvent event) {
+
+	}
+
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
-		
-		
-		
-	}
-	public NoticiaWebController(String url) {
-		 WebEngine webEngine = noticiaWebView.getEngine();
-		
-		webEngine.load(url);
-		
+
+		engine = webView.getEngine();
+		url.addListener((o, ov, nv) -> {
+			loadPage(nv);
+		});
+
 	}
 
-	
-	
+	public void loadPage(String url) {
+		
+		textField.setText(url);
+		engine.load(url);
+	}
+
+	public AnchorPane getView() {
+		return view;
+	}
+
+	public final StringProperty urlProperty() {
+		return this.url;
+	}
+
+	public final String getUrl() {
+		return this.urlProperty().get();
+	}
+
+	public final void setUrl(final String url) {
+		this.urlProperty().set(url);
+	}
 }
